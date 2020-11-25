d8bff6a53c4a619959bd6473c8faa29e24c5d821
stervaq <vlad.starostin.1995@yandex.ru>
2020-11-25 17:43:53 +0300

    Proj

--- app/src/main/java/com/example/mob/MainActivity.kt
+++ app/src/main/java/com/example/mob/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.mob
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-}
+    }
+    var URL = "http://scn-test/freewms_sandbox/ws/TSD.1cws?"
+    var NAMESPACE = "http://tempuri.org/mob"
+    var METHOD_NAME = "mob"
+
+            }
